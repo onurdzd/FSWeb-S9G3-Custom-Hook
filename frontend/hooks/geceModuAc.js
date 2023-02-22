3cc218eee923603a72f1a198049922033825d254
onurd <106928484+onurdzd@users.noreply.github.com>
2023-02-22 20:47:34 +0300

    gecemoduac hook aktif

--- frontend/hooks/geceModuAc.js
+++ frontend/hooks/geceModuAc.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+// import localStorageKullan from "./localStorageKullan";
 
-const geceModuAc = () => {
-  const [toogle, setToogle] = useState(true);
+const geceModuAc = (initialValue) => {
+  const [geceModu, setGeceModu] = useState(initialValue);
+  const toggleDegis = () => setGeceModu(!geceModu);
 
-  return setToggle(!toogle);
+  return [geceModu, toggleDegis];
 };
 
 export default geceModuAc;
---
+++ frontend/hooks/localStorageKullan.js
@@ -0,0 +1,5 @@
+// const localStorageKullan = (keyValue, baslangicDegeri) => {
+//   return <div></div>;
+// };
+
+// export default localStorageKullan;
